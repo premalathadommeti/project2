cc6fcbc206ed16ae170297b5c162ea436ac3cedb
acer <acer@10.0.0.100>
2023-10-06 16:33:41 +0530

    FIRST COMMIT

---
+++ project2/src/com/tnsif/encapsulaltion/encapuslation.java
@@ -0,0 +1,5 @@
+package com.tnsif.encapsulaltion;
+
+public class encapuslation {
+
+}
---
+++ project2/src/com/tnsif/program/constructordemo.java
@@ -0,0 +1,24 @@
+package com.tnsif.program;
+
+import java.util.Scanner;
+
+public class constructordemo {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String name,city;
+		int id;
+		System.out.println("enter id");
+		id=sc.nextInt();
+		sc.nextLine();
+		System.out.println("enter city");
+		city=sc.nextLine();
+		System.out.println("enter name");
+		name=sc.nextLine();
+	
+	}
+	
+}
+
+
