58b54574ba490f7a4aca52c638c1b721945d484e
acer <acer@10.0.0.100>
2023-10-06 16:32:54 +0530

    FIRST COMMIT

---
+++ project2/src/com/tnsif/program/constructor.java
@@ -0,0 +1,14 @@
+package com.tnsif.program;
+
+public class constructor {
+	public String name ;
+	public String city;
+	public int id;
+	public void show() {
+		System.out.println("id"+id+"\t"+"name"+name+"\t"+"city"+city+"\t");
+			
+				
+				
+	}
+
+}
