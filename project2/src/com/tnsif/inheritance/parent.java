814e4d5bf49d81ff7e2593d0bb11bb3bae6f6c8a
acer <acer@10.0.0.130>
2023-10-09 16:52:05 +0530

    first commit

---
+++ project2/src/com/tnsif/inheritance/parent.java
@@ -0,0 +1,24 @@
+package com.tnsif.inheritance;
+
+import com.tnsif.inheritanceprogram.superdemo;
+
+public class parent {
+	int var=100;
+}
+
+public class superdemo extends parent {
+	int var=50;
+	void display () {
+		System.out.println("the child value is"+var);
+		System.out.println("the child value is"+super.var);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		superdemo ob=new superdemo();
+		ob.display();
+		
+
+	}
+
+}
