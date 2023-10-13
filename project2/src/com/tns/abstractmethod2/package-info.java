d0fa23db45eaef76ce923ad3e1267dc32aa122d4
acer <acer@10.0.0.136>
2023-10-13 14:34:45 +0530

    first commit

---
+++ project2/src/com/tns/abstractmethod2/package-info.java
@@ -0,0 +1,1 @@
+package com.tns.abstractmethod2;
---
+++ project2/src/com/tns/interfaceexampletwo/Accounts.java
@@ -0,0 +1,7 @@
+package com.tns.interfaceexampletwo;
+
+public interface Accounts { 
+	void deliverycharges();//abstract methods
+	//int a;//can't declare instance veriables
+
+}
